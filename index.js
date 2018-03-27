1e44e8b2b61f4e6a7334637703eac06ae4214263
Kirsten Land <kirsten.land@balliol.ox.ac.uk>
2018-03-27 22:21:24 +0100

    Added a choice of operators

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const readline = require('readline-sync');
 
 console.log('Welcome to the calculator!');
-console.log('Enter your first number:');
-const num1 = parseFloat(readline.prompt());
-console.log('Enter your second number:');
-const num2 = parseFloat(readline.prompt());
-console.log('Their product is', num1*num2);
+console.log('==========================')
+
+console.log('Please enter the operator: ');
+const operator = readline.prompt();
+
+console.log('Please enter the first number: ');
+const num1_as_string = readline.prompt();
+const num1 = +num1_as_string
+
+console.log('Please enter the second number: ');
+const num2_as_string = readline.prompt();
+const num2 = +num2_as_string
+
+var answer
+switch (operator) {
+    case '+': answer = num1+num2;
+              break;
+    case '-': answer = num1-num2;
+              break;
+    case '*': answer = num1*num2;
+              break;
+    case '/': answer = num1/num2;
+              break;
+}
+
+console.log('The answer is: ' + answer)
