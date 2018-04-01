501f7840a9921688bcb46621d048d92d6a7fb512
Kirsten Land <kirsten.land@balliol.ox.ac.uk>
2018-04-01 17:56:39 +0100

    Cleaned up choice of operations

--- index.js
+++ index.js
@@ -8,22 +8,26 @@ const operator = readline.prompt();
 
 console.log('Please enter the first number: ');
 const num1_as_string = readline.prompt();
-const num1 = +num1_as_string
+const num1 = +num1_as_string;
 
 console.log('Please enter the second number: ');
 const num2_as_string = readline.prompt();
-const num2 = +num2_as_string
+const num2 = +num2_as_string;
 
-var answer
+var answer;
 switch (operator) {
-    case '+': answer = num1+num2;
-              break;
-    case '-': answer = num1-num2;
-              break;
-    case '*': answer = num1*num2;
-              break;
-    case '/': answer = num1/num2;
-              break;
+    case '+':
+        answer = num1+num2;
+        break;
+    case '-':
+        answer = num1-num2;
+        break;
+    case '*':
+        answer = num1*num2;
+        break;
+    case '/':
+        answer = num1/num2;
+        break;
 }
 
 console.log('The answer is: ' + answer)
