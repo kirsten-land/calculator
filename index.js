32588b2d3e17b3fc07347e5df096e1e8c26695b1
Kirsten Land <kirsten.land@balliol.ox.ac.uk>
2018-04-02 17:59:21 +0100

    Refactored using functions

--- index.js
+++ index.js
@@ -2,35 +2,47 @@ const readline = require('readline-sync');
 
 console.log('Welcome to the calculator!');
 console.log('==========================')
+const operator = getInputString('Please enter the operator: ');
+const number = getInputNumber('How many numbers do you want to ' + operator + '?');
+const inputNumbers = getNumbers(number);
+const answer = calculateResults(operator, number, inputNumbers);
+console.log('The answer is: ' + answer)
 
-console.log('Please enter the operator: ');
-const operator = readline.prompt();
-
-console.log('How many numbers do you want to ' + operator + '?');
-const n = readline.prompt();
-
-console.log('Please enter number 1: ');
-var firstInput = readline.prompt();
-var answer = +firstInput;
-
-for (var i = 2; i <= n; i++) {
-    console.log('Please enter number ' + i + ': ');
-    var nextInput = readline.prompt();
-    var nextNum = +nextInput;
-    switch (operator) {
-        case '+':
-            answer += nextNum;
-            break;
-        case '-':
-            answer -= nextNum;
-            break;
-        case '*':
-            answer *= nextNum;
-            break;
-        case '/':
-            answer /= nextNum;
-            break;
-    }
+function getInputString(prompt) {
+    console.log(prompt);
+    return readline.prompt();
 }
 
-console.log('The answer is: ' + answer)
+function getInputNumber(prompt) {
+    return +getInputString(prompt);
+}
+
+function getNumbers(number) {
+    let inputArray = Array(number);
+    for (let i = 0; i < number; i++) {
+        inputArray[i] = getInputNumber('Please enter number ' + (i+1) + ': ');
+    }
+    return inputArray;
+}
+
+function calculateResults(operator, number, inputNumbers) {
+    let answer = inputNumbers[0];
+    for (let i = 1; i < number; i++) {
+        let nextNum = inputNumbers[i];
+        switch (operator) {
+            case '+':
+                answer += nextNum;
+                break;
+            case '-':
+                answer -= nextNum;
+                break;
+            case '*':
+                answer *= nextNum;
+                break;
+            case '/':
+                answer /= nextNum;
+                break;
+        }
+    }
+    return answer;
+}
