ac62773dba2d1a8c76af384f2fcde4c7ef31f2e3
Kirsten Land <kirsten.land@balliol.ox.ac.uk>
2018-06-28 16:25:34 +0100

    First attempt at vowel counting

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
-console.log('==========================');
+const ARITHMETIC_MODE = '1';
+const VOWEL_COUNTING_MODE = '2';
+
+printWelcomeMessage();
 while (true) {
+  const calculationMode = getCalculationMode();
+  if (calculationMode === ARITHMETIC_MODE) {
+    performOneArithmeticCalculation();
+  } else if (calculationMode === VOWEL_COUNTING_MODE) {
+    performOneVowelCountingCalculation();
+  }
+}
+
+function performOneArithmeticCalculation(){
     const operator = getInputString('Please enter the operator: ');
     const number = getInputNumber(`How many numbers do you want to ${operator}?`);
     const inputNumbers = getNumbers(number);
     const answer = calculateResults(operator, number, inputNumbers);
     console.log(`The answer is: ${answer}`);
+}
+
+function performOneVowelCountingCalculation(){
+    const inputString = getInputString("Please enter a string:").toUpperCase();
+    const length = inputString.length;
+    const vowels = ['A', 'E', 'I', 'O', 'U'];
+    let frequencies = new Array(5).fill(0);
+    for (let i = 0; i < length; i++) {
+        for (let vowel = 0; vowel < vowels.length; vowel++) {
+            if (inputString[i] == vowels[vowel]) {
+                frequencies[vowel] += 1;
+            }
+        }
+    }
+    console.log("The vowel counts are:");
+    for (let vowel = 0; vowel < vowels.length; vowel++) {
+        console.log(`${vowels[vowel]}: ${frequencies[vowel]}`);
+    }
+}
+
+function getCalculationMode() {
+    return getInputString(
+    `Which calculator mode do you want?
+     1) Arithmetic
+     2) Vowel counting`);
+}
+
+function printWelcomeMessage() {
+    console.log('Welcome to the calculator!');
+    console.log('==========================');
 }
 
 function getInputString(prompt) {
