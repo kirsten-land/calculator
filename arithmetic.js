72f6dd2734d11815ed05954ead2c4f88343e8bc4
Kirsten Land <kirsten.land@balliol.ox.ac.uk>
2018-06-30 11:58:34 +0100

    Add error checking for operator

--- arithmetic.js
+++ arithmetic.js
@@ -4,8 +4,14 @@ exports.performOneArithmeticCalculation = function(){
     const operator = userInput.getInputString('Please enter the operator: ');
     const number = userInput.getInputNumber(`How many numbers do you want to ${operator}?`);
     const inputNumbers = userInput.getNumbers(number);
-    const answer = calculateResults(operator, number, inputNumbers);
-    console.log(`The answer is: ${answer}`);
+    try {
+        const answer = calculateResults(operator, number, inputNumbers);
+        console.log(`The answer is: ${answer}`);
+    } catch (e) {
+        if (e == "Unknown Operator") {
+            console.log("This operator is not yet known, please try one of +-*/ next time.");
+        }
+    }
 }
 
 function calculateResults(operator, number, inputNumbers) {
@@ -17,6 +23,7 @@ function calculateResults(operator, number, inputNumbers) {
         case "/": operatorFunction = (a,b) => a/b;
                   inputNumbers = [inputNumbers[0]].concat(inputNumbers.slice(1).filter(a => a/=0));
                   break;
+        default:  throw "Unknown Operator";
     }
     const answer = inputNumbers.reduce(operatorFunction);
     return answer;
